8f6e9c8d5b6faee50c4c310a21c0aa09f4ec7365
Vincent Le Goff <vincent@archPC.localdomain>
2021-11-05 21:23:46 +0100

    rewrote Gillespie algo in C++

---
+++ GSSA.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <chrono>
+
+template <size_t nReacs, size_t nSpecies>
+int* Gillespie(int* X, double* K, int (&M)[nReacs][nSpecies], double tstart,
+        double tmax) {
+
+    double t = tstart;
+    double R[nReacs]; // Array of reaction rates
+    double Rsum; // sum of reaction rates
+    double partialRsum; // partial sum of reaction rates
+    int choice; // index of chosen reaction
+    double r1, r2; // random numbers
+    double tau; // increment of time
+    bool exit; // flag to exit the loop
+
+
+    //setting random seed
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    while (t < tmax) {
+
+        Rsum = 0;
+
+        //Calculte reaction rates
+        for (int i=0; i<nReacs; i++) {
+            R[i] = K[i];
+            for (int j=0; j<nSpecies; j++) 
+                if (M[i][j] < 0) R[i] *= pow(X[j], -M[i][j]);
+            Rsum += R[i];
+        }
+
+        exit = true;
+        for (int i=0; i<nReacs; i++) if (R[i] != 0) exit=false; 
+        if (exit) break;
+
+        // Draw two random numbers
+        r1 = (double) std::rand()/RAND_MAX;
+        r2 = (double) std::rand()/RAND_MAX;
+        
+        // Select reaction to fire
+        choice = 0;
+        partialRsum = R[choice];
+        while (partialRsum < r2*Rsum) {
+            choice++;
+            partialRsum += R[choice];
+        }
+        
+        // Pass time
+        tau = -log(r1)/Rsum;
+        t += tau;
+
+        // update X
+        for (int i=0; i<nSpecies; i++) X[i] += M[choice][i];
+
+    }
+
+    return X;
+
+}
+
+int main() {
+
+    int X[3] = {300, 10, 0};
+    int M[2][3] = {{-1, 1, 0}, {0, -1, 1}};
+    double K[2] = {2, .5};
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i=0; i<30000; i++) {
+        X[0] = 300;
+        X[1] = 10;
+        X[2] = 0;
+        Gillespie(X, K, M, 0.0, 10);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto time = end - start;
+
+    for (int i=0; i<3; i++) std::cout << X[i] << " ";
+    std::cout << std::endl;
+    std::cout << "Time taken: " << time/std::chrono::milliseconds(1) << "ms"
+        << std::endl;
+
+    return 0;
+}
