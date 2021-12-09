879af7dfc137c7ff96b68b9b7d964c9b9d932273
Vincent Le Goff <vincent@archPC.localdomain>
2021-12-09 22:05:54 +0100

    adds script and model to validate

---
+++ validation.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+import pandas as pd
+import stochpy
+
+N = 1024;
+
+model = stochpy.SSA()
+model.Model('validation_model.psc', dir='./')
+model.DoStochSim(method='direct', trajectories=N, mode='time', end=1.0)
+
+distrib = np.zeros((N, 3))
+for i in range(N):
+    model.GetTrajectoryData(i+1)
+    distrib[i, :] = model.data_stochsim.species[-1]
+
+df = pd.DataFrame(data={'S1': distrib[:, 0],
+                        'S2': distrib[:, 1],
+                        'S3': distrib[:, 2],
+                        'method': 'stochpy'})
+
+os.system('g++ ./GSSA.cpp -O3 -o ./GSSA')
+os.system(f'./GSSA networks/test.txt {N}')
+
+for i, line in enumerate(open('./results.txt', 'r')):
+    line = line[:-1].split(',')
+    for j in range(3):
+        distrib[i, j] = int(line[j])
+
+df2 = pd.DataFrame(data={'S1': distrib[:, 0],
+                         'S2': distrib[:, 1],
+                         'S3': distrib[:, 2],
+                         'method': 'C++'})
+
+os.system('nvcc ./GSSA.cu -o GSSA_cu')
+os.system(f'nvprof ./GSSA_cu networks/test.txt {N}')
+
+for i, line in enumerate(open('./results.txt', 'r')):
+    line = line[:-1].split(',')
+    for j in range(3):
+        distrib[i, j] = int(line[j])
+
+df3 = pd.DataFrame(data={'S1': distrib[:, 0],
+                         'S2': distrib[:, 1],
+                         'S3': distrib[:, 2],
+                         'method': 'CUDA'})
+
+df = df.append(df2)
+df = df.append(df3)
+
+df.to_csv('validation.csv')
