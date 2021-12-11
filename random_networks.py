eb0024075bc2b956bbc724afd7b391b482b08758
Vincent Le Goff <vb.legoff@free.fr>
2021-12-11 19:21:51 +0100

    add more random networks

--- random_networks.py
+++ random_networks.py
@@ -6,13 +6,13 @@ path = "./networks"
 if not os.path.exists(path):
     os.makedirs(path)
 
-Nspecies = np.array([100, 500, 1000, 5000, 10000])
+Nspecies = np.array([10, 20, 100, 500, 1000])
 
 for i, Nsp in enumerate(Nspecies):
     print(i)
     X = np.random.randint(0, 10000, size=Nsp)
-    Nreacs = int(.2*Nsp)
-    K = np.random.rand(Nreacs)*5
+    Nreacs = int(.8*Nsp)
+    K = np.random.rand(Nreacs)*1
     M = np.zeros((Nreacs, Nsp))
     # Generating stoechiometry matrix
     for i in range(Nreacs):
@@ -30,6 +30,8 @@ for i, Nsp in enumerate(Nspecies):
             choices = np.random.choice(np.arange(Nsp), 2)
             for j in choices:
                 M[i][j] = 1
+    M = np.unique(M, axis=0)
+    Nreacs = M.shape[0]
     # writing network
     with open(f"{path}/network{Nsp}.txt", "w") as f:
         # writing species
@@ -38,6 +40,7 @@ for i, Nsp in enumerate(Nspecies):
             f.write(f"{X[i]} ")
         f.write('\n')
         # writing reaction rates
+        print(Nreacs)
         f.write(f"{Nreacs} ")
         for i in range(Nreacs):
             f.write(f"{K[i]} ")
@@ -47,4 +50,3 @@ for i, Nsp in enumerate(Nspecies):
             for j in range(Nsp):
                 f.write(f"{M[i][j]} ")
         f.write('\n')
-
